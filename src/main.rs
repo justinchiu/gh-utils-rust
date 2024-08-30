77dc5618cb6d248778cc4b5a81a9ba86356ac430
justinchiu-cohere <justinchiu@cohere.com>
2024-08-30 14:51:45 -0400

    test data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 #[tokio::main]
 async fn main() -> octocrab::Result<()> {
+    use std::time::Instant;
+    let start = Instant::now();
+    let mut x = 0_u32;
+    for i in 1..2_u32.pow(20) {
+        x += 1;
+    }
+    let end = Instant::now();
+    let duration = (end-start).as_secs_f64();
+    println!("{duration}");
+
     let diff = octocrab::instance()
         .pulls("sqlfluff", "sqlfluff")
         .get_diff(1625)
