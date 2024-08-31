66e5cda6c4531e2e748d84d5e771f0f7e3e8e906
justinchiu-cohere <justinchiu@cohere.com>
2024-08-31 05:41:34 +0000

    octocrab doesnt actually clone repos, so i cant use it

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use csv::Reader;
 use csv::StringRecord;
-use octocrab::Octocrab;
 use std::path::Path;
 use std::time::Instant;
+use octocrab::Octocrab;
+use git2::Repository;
+
 
 fn get_owner_repo(record: &StringRecord) -> (&str, &str) {
     let parts: Vec<&str> = record.get(0).unwrap().split('/').collect();
@@ -23,18 +25,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = std::fs::File::open(file_path)?;
     let mut reader = Reader::from_reader(file);
 
+
+    std::fs::create_dir("./repos")?;
     let headers = reader.headers()?.clone();
     println!("Header: {:?}", headers);
     for (index, result) in reader.records().enumerate() {
         let record = result?;
         println!("Record: {:?}", record);
+        let fullrepo = record.get(0).unwrap();
+        let url = "https://github.com/{fullrepo}";
+        let (owner, reponame) = get_owner_repo(&record);
+        println!("{owner} {reponame}");
+        let repo = match Repository::clone(url, "./repos/{reponame}") {
+            Ok(repo) => repo,
+            Err(e) => panic!("failed to clone: {}", e),
+        };
 
-        let (owner, repo) = get_owner_repo(&record);
-        println!("{owner} {repo}");
 
-        let content = octocrab.repos(owner, repo).get_content().send().await?;
+
+        let content = octocrab.repos(owner, reponame).get_content().send().await?;
         let num_files = content.items.len();
-        println!("{num_files} files/dirs in the repo {owner}/{repo}");
+        println!("{num_files} files/dirs in the repo {owner}/{reponame}");
 
         println!("{:?}", content.items[0]);
 
