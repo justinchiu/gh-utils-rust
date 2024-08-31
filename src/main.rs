08f83bdb9a40db8fd5520992db79b8e79995e2e1
justinchiu-cohere <justinchiu@cohere.com>
2024-08-31 00:40:28 +0000

    get repo contents. but contents are just file links

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,58 @@
-#[tokio::main]
-async fn main() -> octocrab::Result<()> {
-    use std::time::Instant;
-    let start = Instant::now();
-    let mut x = 0_u32;
-    for i in 1..2_u32.pow(20) {
-        x += 1;
+use std::path::Path;
+use std::time::Instant;use csv::Reader;
+use csv::StringRecord;
+use octocrab::Octocrab;
+
+
+fn get_owner_repo(record: &StringRecord) -> (&str, &str) {
+    let parts: Vec<&str> = record.get(0).unwrap().split('/').collect();
+    if parts.len() != 2 {
+        panic!("Invalid input format. Expected 'owner/repo'.");
     }
+    (parts[0], parts[1])
+}
+
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let start = Instant::now();
+
+    let token = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env variable is required");
+    let octocrab = Octocrab::builder().personal_token(token).build()?;
+
+    let file_path = Path::new("mydata/data.csv");
+    let file = std::fs::File::open(file_path)?;
+    let mut reader = Reader::from_reader(file);
+
+    let headers = reader.headers()?.clone();
+    println!("Header: {:?}", headers);
+    for (index, result) in reader.records().enumerate() {
+        let record = result?;
+        println!("Record: {:?}", record);
+
+        let (owner, repo) = get_owner_repo(&record);
+        println!("{owner} {repo}");
+
+
+        let content = octocrab
+            .repos(owner, repo)
+            .get_content()
+            .send()
+            .await?;
+        let num_files = content.items.len();
+        println!("{num_files} files/dirs in the repo {owner}/{repo}");
+
+        println!("{:?}", content.items[0]);
+
+        if index == 5 {
+            break;
+        }
+    }
+
     let end = Instant::now();
-    let duration = (end-start).as_secs_f64();
-    println!("{duration}");
-
-    let diff = octocrab::instance()
-        .pulls("sqlfluff", "sqlfluff")
-        .get_diff(1625)
-        .await?;
-
-    println!("{diff}");
+    let duration = (end - start).as_secs_f64();
+    println!("Duration: {duration}");
 
     Ok(())
 }
